53bd09d019fb2007c9049d13cc63ba39979afb21
Arun mohan k <arunmohankrl@email.com>
2026-06-19 13:27:36 +0530

    fix: pass-through sw.js, purge all caching on activate

--- template/sw.js
+++ template/sw.js
@@ -1,36 +1,16 @@
-const CACHE_NAME = 'srm-match-v1.0';
-const STATIC_ASSETS = [
-  '/manifest.json',
-  '/static/favicon.png'
-];
-
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(STATIC_ASSETS);
-    })
-  );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      );
-    })
-  );
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map(key => caches.delete(key)));
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    // Pass-through strategy to bypass the cache entirely
+    event.respondWith(fetch(event.request));
 });
